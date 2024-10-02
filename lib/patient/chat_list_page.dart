a43786d4ac3f3ca79abafac62fea16cd2ac71e2a
Mohit Khairnar <mohitkhairnar28@gmail.com>
2024-10-02 14:34:35 +0530

    Appointment Booking Bug Fix

--- lib/patient/chat_list_page.dart
+++ lib/patient/chat_list_page.dart
@@ -124,7 +124,7 @@ class _ChatListPageState extends State<ChatListPage> {
                                     padding: const EdgeInsets.only(
                                         left: 16.0, right: 20.0),
                                     child: Text(
-                                      '${doctor.firstName} ${doctor.lastName}',
+                                      'Dr. ${doctor.firstName} ${doctor.lastName}',
                                       style: GoogleFonts.nunitoSans(
                                           fontSize: 17,
                                           fontWeight: FontWeight.w600),
